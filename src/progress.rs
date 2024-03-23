8c611a3d6b46f5da7ff18d543a51150576abf112
mrudisel <mitchrudisel@gmail.com>
2024-03-23 14:28:11 -0700

    add progress helpers

--- src/progress.rs
+++ src/progress.rs
@@ -1,4 +1,7 @@
-use std::{path::Path, sync::Arc};
+use std::path::Path;
+use std::sync::atomic::Ordering::Relaxed;
+use std::sync::atomic::{AtomicBool, AtomicU64};
+use std::sync::Arc;
 
 pub trait DlProgress {
     fn start(&mut self, path: &Path, total_bytes: Option<u64>);
@@ -36,12 +39,10 @@ impl<P: DlProgress + ?Sized> DlProgress for Box<P> {
     }
 }
 
-
-
 impl<P> DlProgress for Arc<P>
 where
     P: ?Sized,
-    for<'a> &'a P: DlProgress,    
+    for<'a> &'a P: DlProgress,
 {
     fn start(&mut self, path: &Path, total_bytes: Option<u64>) {
         <&P as DlProgress>::start(&mut &**self, path, total_bytes)
@@ -55,7 +56,6 @@ where
         <&P as DlProgress>::finished(&mut &**self, path)
     }
 }
-
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct ProgressContainer<
@@ -101,5 +101,112 @@ where
         if let Some(finish_fn) = self.finish_fn.take() {
             (finish_fn)(&mut self.ctx, path);
         }
+    }
+}
+
+impl<Ctx, S, U, F> DlProgress for &ProgressContainer<Ctx, S, U, F>
+where
+    S: Fn(&Ctx, &Path, Option<u64>),
+    U: Fn(&Ctx, &Path, u64),
+    F: Fn(&Ctx, &Path),
+{
+    fn start(&mut self, path: &Path, total_bytes: Option<u64>) {
+        if let Some(ref start_fn) = self.start_fn {
+            (start_fn)(&self.ctx, path, total_bytes);
+        }
+    }
+
+    fn update(&mut self, path: &Path, bytes_written: u64) {
+        (self.update_fn)(&self.ctx, path, bytes_written);
+    }
+
+    fn finished(&mut self, path: &Path) {
+        if let Some(ref finish_fn) = self.finish_fn {
+            (finish_fn)(&self.ctx, path);
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct ProgressHandle<F = fn(&Path, u64, Option<u64>, DlState)> {
+    total_bytes: AtomicU64,
+    bytes_written: AtomicU64,
+    finished: AtomicBool,
+    caller: F,
+}
+
+impl<F> ProgressHandle<F> {
+    pub const fn new(caller: F) -> Self {
+        Self {
+            total_bytes: AtomicU64::new(0),
+            bytes_written: AtomicU64::new(0),
+            finished: AtomicBool::new(false),
+            caller,
+        }
+    }
+
+    pub fn state(&self) -> DlState {
+        if self.is_finished() {
+            DlState::Finished
+        } else if self.get_bytes_written() == 0 {
+            DlState::Starting
+        } else {
+            DlState::Running
+        }
+    }
+
+    pub fn is_finished(&self) -> bool {
+        self.finished.load(Relaxed)
+    }
+
+    pub fn get_bytes_written(&self) -> u64 {
+        self.bytes_written.load(Relaxed)
+    }
+
+    pub fn get_total_bytes(&self) -> Option<u64> {
+        match self.total_bytes.load(Relaxed) {
+            0 => None,
+            non_zero => Some(non_zero),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum DlState {
+    Starting,
+    Running,
+    Finished,
+}
+
+impl<F> DlProgress for &ProgressHandle<F>
+where
+    F: Fn(&Path, u64, Option<u64>, DlState),
+{
+    fn start(&mut self, path: &Path, total_bytes: Option<u64>) {
+        if let Some(total) = total_bytes {
+            self.total_bytes.store(total, Relaxed);
+        }
+
+        (self.caller)(path, 0, total_bytes, DlState::Starting);
+    }
+
+    fn update(&mut self, path: &Path, bytes_written: u64) {
+        let max = self
+            .bytes_written
+            .fetch_max(bytes_written, Relaxed)
+            .max(bytes_written);
+
+        (self.caller)(path, max, self.get_total_bytes(), DlState::Running);
+    }
+
+    fn finished(&mut self, path: &Path) {
+        self.finished.store(true, Relaxed);
+
+        (self.caller)(
+            path,
+            self.get_bytes_written(),
+            self.get_total_bytes(),
+            DlState::Finished,
+        );
     }
 }
